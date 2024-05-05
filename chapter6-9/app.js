dce8001f10d9f3e74caec78bacd0ebba4476d974
FizzaChouhan <chouhanfizza@gmail.com>
2024-05-05 20:27:03 +0500

    chap6-9

---
+++ chapter6-9/app.js
@@ -0,0 +1,72 @@
+// var a = 10 ;
+
+// document.write("<h1>Result</h1>")
+// document.write("Value of a is")
+// document.write(a)
+// +a;
+// document.write("</br>")
+// document.write("the value of ++a is")
+// document.write(++a)
+// a++;
+// document.write("</br>")
+// document.write("Value of a++")
+// document.write(a++)
+// --a;
+// document.write("</br>")
+// document.write("the value of --a")
+// document.write(--a)
+// a--;
+// document.write("</br>")
+// document.write("Value of a-- is ")
+// document.write(a--)
+
+// var a = 2;
+// document.write("a is")
+// document.write(a)
+// var b=1;
+// document.write("</br>")
+// document.write("b is")
+// document.write(b)
+// var result = --a - --b + ++b + b--;
+// document.write("</br>")
+// document.write("result is")
+// document.write(result)
+// --a;
+// console.log(--a);
+// --a - --b ;
+// console.log(--a - --b );
+// --a - --b + ++b;
+// console.log(--a - --b + ++b)
+// --a - --b + ++b + b--
+// console.log(--a - --b + ++b + b--)
+
+
+// prompt("What's your name?");
+
+
+// var num = prompt ("user number" ,"0");
+// var num = parseInt(num); 
+// var i = 0;
+// document.write("<table border="1" cellspacing="0">");
+// for (i = 1; i < 10; i++) {
+//     document.write("<tr><td>" + num + " x " + i + " = " + num * i + "</td></tr>");
+//   }
+
+// var sub1="maths";
+// var sub2="physics";
+// var sub3="urdu";
+// var totalMarks=100;
+// var obtainedMarks1 = 54;
+// var obtainedMarks2 = 54;
+// var obtainedMarks3 = 48;
+// var sumTotal = totalMarks*3;
+// var sumObtained = obtainedMarks1+obtainedMarks2+obtainedMarks3;
+// var percenage1=(obtainedMarks1/totalMarks)*100;
+// var percenage2=(obtainedMarks2/totalMarks)*100;
+// var percenage3=(obtainedMarks3/totalMarks)*100;
+// var tp=52;
+// document.write("<table>")
+// document.write (`<tr><td> ${sub1} </td><td> ${totalMarks} </td><td> ${obtainedMarks1} </td><td> ${percenage1} </td></tr>`)
+// document.write (`<tr><td> ${sub2} </td><td> ${totalMarks} </td><td> ${obtainedMarks2} </td><td> ${percenage2} </td></tr>`)
+// document.write (`<tr><td> ${sub3} </td><td> ${totalMarks} </td><td> ${obtainedMarks3} </td><td> ${percenage3} </td></tr>`)
+// document.write (`<tr><td> ${sumTotal} </td><td> ${sumObtained} </td><td> ${tp} </td></tr>`)
