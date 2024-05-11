8b91e54f1eab4cacaec9662b8bfa0bc393466596
FizzaChouhan <chouhanfizza@gmail.com>
2024-05-11 11:26:58 +0500

    incomplete

---
+++ chapter 9-11/app.js
@@ -0,0 +1,96 @@
+
+    //question1
+    // var city = prompt("What's your city name");
+    // if (city === "karachi"){
+    // alert("Welcome to city of lights");
+    // }
+
+    //question2
+//    var gender = prompt("what's youe gender");
+//    if (gender="female"){
+//     alert("Good Morning Ma'am")
+//    }
+//    if (gender="male"){
+//     alert("Good Morning Sir")
+//    }
+
+    //question3
+    // var signal = prompt("What's is color of traffic road signals");
+    // if (signal === "red"){
+    //     alert("must stop");
+    // }
+    // if (signal === "yellow"){
+    //     alert("ready to move");
+    // }
+    // if (signal === "green"){
+    //     alert("move now");
+    // }
+
+    //question4
+    // var fuel = prompt("remaining fuel in car");
+    // if (fuel<0.25){
+    //     alert("Please refill the fuel in the car")
+    // }
+
+    //question5
+    //a
+//     var a = 4;
+// if (++a === 5){
+// alert(a);
+// }
+//true
+
+//b
+// var b = 82;
+// if (b++ === 83){
+// alert(b);
+// }
+//false
+
+//c
+ var c = 12;
+// if (c++ === 13){
+// alert(c);
+// }
+//false
+// if (c === 13){
+//     alert("c");
+//     }
+   //false 
+//    if (++c < 14){
+//     alert(c);
+//     } 
+    //true
+    // if(c === 14){
+    //     alert(c);
+    //     }   
+//false
+
+//d
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert(totalCost);
+// }
+ //true  
+ 
+ //e
+//  if (true){
+//     alert("True");
+//     }
+    //true
+    // if (false){
+    //     alert("False");
+    //     }
+    //false 
+    
+    //f
+    // if("car" < "cat"){
+    //     alert("car is smaller than cat");
+    //     }
+     //true 
+     
+     
+     //question6
+     var obtMark1 = prompt ("write marks of physics")      
