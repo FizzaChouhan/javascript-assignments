9bc223cc81e7409357107417cc3671d5d6a72169
FizzaChouhan <chouhanfizza@gmail.com>
2024-05-19 18:18:59 +0500

    chap 12

---
+++ chapter 12-13/app.js
@@ -0,0 +1,47 @@
+//question 1
+// var userData=prompt("enter your data");
+// if (userData >= "A" && userData <= "Z"){
+//     document.write(userData+" is an upper case character </br>");
+// }
+// else if (userData >= "a" && userData <= "z"){
+//     document.write(userData+" is an lower case character </br>");
+// }
+// else { 
+//     document.write(userData+" is not an alphabetic character ")
+// }
+
+//question 2
+// var integer1 = +prompt("enter a integer1");
+// var integer2 = +prompt("enter a integer2");
+
+// if (integer1>integer2){
+//     alert(integer1);
+// }
+// if (integer2>integer1){
+//     alert(integer2);
+// }
+// if (integer2==integer1){
+//     alert(integer2);
+// }
+
+//question 3 
+// var num = +prompt("enter any number");
+// if (num>0){
+//     alert("positive");
+// }
+// else if  (num==0){
+//     alert("number is zero");
+// }
+// else {
+//     alert("number is negative")
+// }
+
+//question 4
+var character = prompt("write a character");
+var vowels="a,e,i,o,u";
+if (character==vowels){
+    alert("true")
+}
+else{
+    alert("false")
+}
