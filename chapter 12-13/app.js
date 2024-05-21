d489f36afdbd29ef3f2eba138b32afbb1f331968
FizzaChouhan <chouhanfizza@gmail.com>
2024-05-21 22:13:42 +0500

    sec

--- chapter 12-13/app.js
+++ chapter 12-13/app.js
@@ -37,11 +37,70 @@
 // }
 
 //question 4
-var character = prompt("write a character");
-var vowels="a,e,i,o,u";
-if (character==vowels){
-    alert("true")
-}
-else{
-    alert("false")
-}
+// var character = prompt("write a character");
+// var vowels1="a";
+// var vowels2="e";
+// var vowels3="i";
+// var vowels4="o";
+// var vowels5="u";
+// if (character==vowels1){
+//     alert("true")
+// }
+//  else if (character==vowels2){
+//     alert("true")
+// }
+// else if (character==vowels3){
+//     alert("true")
+// }
+// else if (character==vowels4){
+//     alert("true")
+// }
+// else if (character==vowels5){
+//     alert("true")
+// }
+// else{
+//     alert("false")
+// }
+
+//question5
+//5. Write a program that
+//a. Store correct password in a JS variable.
+//b. Asks user to enter his/her password
+//c. Validate the two passwords:
+//i. Check if user has entered password. If not, then
+//give message “ Please enter your password”
+//ii. Check if both passwords are same. If they are
+//same, show message “Correct! The password you
+//entered matches the original password”. Show
+//“Incorrect password” otherwise.
+
+// var userPassword = +prompt("Enter your password");
+
+// var password =12345678 ;
+// if (userPassword===password){
+//     alert("“Correct! The password you entered matches the original password”.");
+// }
+// else if (userPassword===""){
+//     alert(" Please enter your password")
+// }
+// else if (userPassword!==password) {
+//     alert("“Incorrect password”")
+// }
+
+//question6
+//6. This if/else statement does not work. Try to fix it:
+//var greeting;
+//var hour = 13;
+//if (hour < 18) {
+//greeting = "Good day";
+//else
+//greeting = "Good evening";
+//}
+// var hour = +prompt("what time is it");
+// var hour =  13;
+// if (hour==13 && hour<18){
+//     alert("Good day");
+// }
+// else{
+//     alert("Good evening");
+// }
