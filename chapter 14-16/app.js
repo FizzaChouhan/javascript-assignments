bf8a4ca0670bdc49208a29323d7a9b95ddffa8ae
FizzaChouhan <chouhanfizza@gmail.com>
2024-05-25 13:48:23 +0500

    chap14-16

---
+++ chapter 14-16/app.js
@@ -0,0 +1,53 @@
+//question1
+//1. Declare an empty array using JS literal notation to store
+//student names in future.
+// var studentName = [];
+
+//question2
+// 2. Declare an empty array using JS object notation to store
+// student names in future.
+//var studentName = {};
+
+//question3
+//3. Declare and initialize a strings array.
+// var stringArray = ["Apple","Banana","Mango"];
+// console.log(stringArray);
+
+//question4
+//4. Declare and initialize a numbers array.
+// var numArray = [1,2,3,4,5];
+// console.log(numArray)
+
+//question5
+//5. Declare and initialize a boolean array.
+// var booleanArray = [true,false];
+// console.log(booleanArray);
+
+//question6
+//6. Declare and initialize a mixed array.
+// var mixedArray = ["Apple",1,3,true,false];
+// console.log(mixedArray);
+
+//question7
+//7. Declare and Initialize an array and store available
+// education qualifications in Pakistan (e.g. SSC, HSC, BCS,
+//     BS, BCOM, MS, M. Phil., PhD). Show the listed
+//     qualifications in your browser like:
+// var qualifications = ["SSC","HSC","BSC","BS","BCOM","MS","M.Phil","Phd"];
+// document.write("Qualification:");
+// document.write("</br>");
+// document.write(1. + qualifications[0]);
+// document.write("</br>");
+// document.write(2. + qualifications[1]);
+// document.write("</br>");
+// document.write(3. + qualifications[2]);
+// document.write("</br>");
+// document.write(4. + qualifications[3]);
+// document.write("</br>");
+// document.write(5. + qualifications[4]);
+// document.write("</br>");
+// document.write(6. + qualifications[5]);
+// document.write("</br>");
+// document.write(7. + qualifications[6]);
+// document.write("</br>");
+// document.write(8. + qualifications[7]);
