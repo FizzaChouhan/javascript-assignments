547b911daa432c5696d4c5426ce2ffa569c771ec
FizzaChouhan <chouhanfizza@gmail.com>
2024-07-07 20:50:11 +0500

    chap

---
+++ chap 26-30/app.js
@@ -0,0 +1,65 @@
+// 1. Write a program that takes a positive integer from user &
+// display the following in your browser.
+// a. number
+// b. round off value of the number
+// c. floor value of the number
+// d. ceil value of the number
+
+// var userNum = prompt("Enter a number")
+// var roundOff = Math.round(userNum)
+// var floor = Math.floor(userNum)
+// var ceil = Math.ceil(userNum)
+// document.write("Number: " + userNum + "<br>")
+// document.write("Round off value: " + roundOff + "<br>")
+// document.write("Floor value: " + floor + "<br>")
+// document.write("Ceil value: " + ceil + "<br>")
+
+
+// 2. Write a program that takes a negative floating point
+// number from user & display the following in your browser.
+// a. number
+// b. round off value of the number
+// c. floor value of the number
+// d. ceil value of the number
+
+// var userNum = prompt("Write a negative integer");
+// var roundOff = Math.round(userNum);
+// var floor = Math.floor(userNum);
+// var ceil = Math.ceil(userNum);
+// document.write("Number: " + userNum + "<br>")
+// document.write("Round off value: " + roundOff + "<br>")
+// document.write("Floor value: " + floor + "<br>")
+// document.write("Ceil value: " + ceil + "<br>")
+
+// 3. Write a program that displays the absolute value of a
+// number.
+// E.g. absolute value of -4 is 4 & absolute value of 5 is 5
+// var userNum = prompt("Write a number")
+// var abs = Math.abs(userNum)
+// document.write("Absolute value: " + abs + "<br>")
+
+// 4. Write a program that simulates a dice using random()
+// method of JS Math class. Display the value of dice in your
+// browser.:
+// var dice = Math.random()
+// dice = dice * 6
+// dice = Math.floor(dice)
+// document.write("Random dice value: " + dice + "<br>")
+
+// 5. Write a program that simulates a coin toss using random()
+// method of JS Math class. Display the value of coin in your
+// browser
+//  var coin = Math.random()
+//  coin = coin * 2
+//  coin = Math.floor(coin)
+//  if(coin=2){
+//     document.write("coin value="+coin+"Head")
+//  }
+//  else{
+//     document.write("coin value="+coin+"Tail")
+//  }
+//  document.write("Random coin value: " + coin + "<br>")
+
+
+
+
