ff477a6e90e8582c0b56435b7333bf6efb676f04
FizzaChouhan <chouhanfizza@gmail.com>
2024-07-07 21:57:09 +0500

    assignment

---
+++ chapter 31-34/app.js
@@ -0,0 +1,69 @@
+// 1. Write a program that displays current date and time in
+// your browser.
+
+//  var today = new Date();
+//  document.write(today)
+
+// 2. Write a program that alerts the current month in words.
+// For example December.
+//  var date = new Date();
+//  var month = date.getMonth()
+//  document.write("Month:"+month)
+
+// 3. Write a program that alerts the first 3 letters of the current
+// day, for example if today is Sunday then alert will show
+// Sun.
+// var date = new Date()
+// var day = date.getDay()
+// var firstThree = day.slice(0,3)
+// document.write("Today is "+firstThree+"day")
+
+// 5.Write a program that shows the message “First fifteen
+// days of the month” if the date is less than 16th of the month
+// else shows “Last days of the month”.
+//  var date = new Date()
+//  var day = date.getDate()
+//  if(day<16){
+//     document.write("First fifteen days of the month")
+//     }else{
+//         document.write("Last days of the month")
+//         }
+
+// 6. Write a program that determines the minutes since
+// midnight, Jan. 1, 1970 and assigns it to a variable that
+// hasn't been declared beforehand. Use any variable you like
+// to represent the Date object.
+//  var date = new Date()
+
+//  var millisec = date.getTime()
+//  var minutes = millisec*60*60
+//  document.write("Current Date: "+date+"<br>")
+//  document.write("Elapsed milliseconds since January 1, 1970: "+millisec+"<br>")
+//  document.write("Elapsed minutes since January 1, 1970: "+minutes+"<br>")
+
+// 7. Write a program that tests whether it's before noon and
+// alert “Its AM” else “its PM”.
+//   var date = new Date()
+//   var hour = date.getHours()
+//   if(hour<12){
+//     document.write("Its AM")
+//     }else{
+//         document.write("Its PM")
+//         }
+
+// 8. Write a program that creates a Date object for the last day
+// of the last month of 2020 and assigns it to variable named
+// laterDate.
+  //var date = new Date("Dece")
+//   var laterDate = new Date("December 2020 31")
+  
+//   document.write("Later Date: "+laterDate+"<br>")
+
+
+
+
+
+
+
+
+
