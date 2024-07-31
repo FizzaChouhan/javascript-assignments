7166f1499c5f4df73304206be665494cb8db7331
FizzaChouhan <chouhanfizza@gmail.com>
2024-07-31 17:46:15 +0500

    final

---
+++ 39-42 chap/app.js
@@ -0,0 +1,26 @@
+// 1. Write a custom function power ( a, b ), to calculate the value of a raised to b.
+// function power(a,b){
+//     return a**b;
+//     }
+//     console.log(power(2,3));
+
+// 2. Any year is entered through the keyboard. Write a function to determine whether the year is a leap year or not.
+//  function leapYear(year){
+//     var year=+prompt("Write a year")
+//     switch(year){
+//       case 2012:
+//       alert("it's a leap year") ;
+//       break;
+//       case 2016:
+//       alert("it's a leap year") ;
+//       break;
+//       case 2020:
+//       alert("it's a leap year") ;
+//       break;
+
+
+
+    }
+   
+
+    
---
+++ chap43-48/app.js
@@ -0,0 +1,85 @@
+// 1. Show an alert box on click on a link.
+// function hello (){
+//     alert("Hello World!");
+// }
+// 2. Display some Mobile images in browser. On click on an
+// image Show the message in alert to user.
+// function purchasing (){
+//     alert("Thanks to purchase")
+// }
+
+// 3. Display 10 student records in table and each row should contain a delete
+// button. If you click on a button to delete a record, entire row should be
+// deleted.
+//  function deleteRecord1 (){
+//    var table= document.getElementById("myTable").deleteRow(1);
+//      alert("Record deleted");
+//      }
+//      function deleteRecord2 (){
+//         var table= document.getElementById("myTable").deleteRow(2);
+//           alert("Record deleted");
+//           }
+//           function deleteRecord3 (){
+//             var table= document.getElementById("myTable").deleteRow(3);
+//               alert("Record deleted");
+//               }
+//               function deleteRecord4 (){
+//                 var table= document.getElementById("myTable").deleteRow(4);
+//                   alert("Record deleted");
+//                   }
+
+// 4. Display an image in browser. Change the picture on mouseover and set the
+// first picture on mouseout.
+
+// 5. Show a counter in browser. Counter should increase on click on increase
+// button and decrease on click on decrease button. And show updated counter
+// value in browser.
+// var counter = 0
+
+// function increase() {
+
+//    document.getElementById("counter").innerText = counter++
+// }
+// function decrease() {
+
+//    if(counter<0){
+//       counter = 0
+//    }
+//    document.getElementById("counter").innerText = counter--
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var counter=0;
+//  function increase(){
+//    for(var i=0;i>counter.lenght;i++){
+//       counter++;
+//     alert("the value"+counter)  
+//    }
+
+//     counter++;
+//     alert("the value"+counter)
+//  }
+//  function decrease(){
+
+//    counter--;
+//    alert("the value"+counter)
+// }
+
+
+
+  
+
+
